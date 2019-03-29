7a7b107667b741457add2671056db1da610a1ca4
Raúl <raul@raulhernandez.net>
2019-03-29 22:36:47 +0100

    Initial commit

---
+++ src/tests/example1.js
@@ -0,0 +1,18 @@
+var utils = require('../utils/utils.js');
+module.exports = {
+    'Chancejs demo test' : function (client) {
+      const nameToSearch = utils.generate_random_name();
+      client
+        .url('http://www.google.com')
+        .waitForElementVisible('body', 2000)
+        .assert.title('Google')
+        .assert.visible('input[type=text]')
+        .setValue('input[type=text]', nameToSearch)
+        .pause(1000)
+        .waitForElementVisible('input[name="btnK"]', 2000)
+        .click('input[name="btnK"]')
+        .waitForElementVisible('#search', 2000)
+        .assert.containsText('#search', nameToSearch)
+        .end();
+    }
+  };
